c369a7d6ab16c79337948e373c6e83af794876ef
Rafsanfaruk <39384467+Rafsanfaruk@users.noreply.github.com>
2023-05-21 12:46:09 +0600

    added toy get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port =process.env.PORT || 5000;
@@ -14,7 +14,6 @@ app.get('/',(req, res)=>{
     res.send('Toy Tinkerers is Running')
 })
 
-console.log(process.env.TOY_PASS)
 
 // mongodb code  start
 
@@ -35,8 +34,51 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+   const toyCollection =client.db('toyTinkerers').collection('allToys');
+   const addToyCollection =client.db('toyTinkerers').collection('addToys');
+
+// 
+   
+// 
+   app.get('/allToys',async(req,res)=>{
+    const cursor =toyCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
 
 
+   app.get('/category',async(req,res)=>{
+    const cursor =toyCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
+
+
+   app.get('/allToys/:id',async(req,res)=>{
+     const id =req.params.id;
+     const query ={ _id: new ObjectId(id)}
+     const result  = await toyCollection.findOne(query);
+     res.send(result);
+   })
+
+// add Toy
+app.get('/addToys',async(req,res)=>{
+  const result =await addToyCollection.find().toArray();
+  res.send(result);
+})
+
+
+app.post('/addToys', async (req, res) => {
+  try {
+    const addedToys = req.body;
+    console.log(addedToys);
+    const result = await addToyCollection.insertOne({ ...addedToys, _id: undefined });
+    res.status(201).json({ success: true, message: 'Toy added successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Failed to add toy' });
+  }
+});
 
 
 
